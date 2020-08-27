d21343ab1af1f7022ce1c935bdd1326c9a453c56
Stephen Nuguid <stephen.nuguid@my.tccd.edu>
2020-08-26 20:54:44 -0500

    Playing around with debugging

--- classwork/HelloWorld/HelloWorld/Program.cs
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿/* 
+ * Stephen Nuguid
+ * ITSE 1430
+ * Lab 1
+ */
+using System;
 
 namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main ( string[] args )
         {
+            // A single line comment
+            // Another line of comments
             Console.WriteLine("Hello World!");
+
+            int hours = 40;
+            double payRate = 12.80;
+
+            double totalPay = hours * payRate;
+
+            // Another block
+            // 10 + 45;
+            // NO!!!! { Console.Writeline(); }
         }
     }
 }
