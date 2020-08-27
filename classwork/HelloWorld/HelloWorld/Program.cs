4eeecbbc3f617c619a71dac9096bdc5d136f3ab7
Stephen Nuguid <stephen.nuguid@my.tccd.edu>
2020-08-26 21:35:31 -0500

    Playing around with C#

--- classwork/HelloWorld/HelloWorld/Program.cs
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -7,22 +7,47 @@ using System;
 
 namespace HelloWorld
 {
+    // Pascal casing - Capitalize on word boundaries including first word
+    // Camel casing - Capitalize on word boundaries except first word (e.g firstName, payRate)
     class Program
     {
-        static void Main ( string[] args )
+        // function declaration - declaration to compiler that a function exist with a given signature
+        // Function signature - fuction name and the parameter types (sometimes it will include return type)
+        // Function definition - declaration + implementation 
+        static void Main ( string[] args )  // 1 parameter 
         {
             // A single line comment
             // Another line of comments
-            Console.WriteLine("Hello World!");
 
-            int hours = 40;
-            double payRate = 12.80;
+            // Display given string to output
+            // Arguments - data passed to function
+            Console.WriteLine("Hello World!"); //printf, cout
 
-            double totalPay = hours * payRate;
+            // Variable declaration:
+            // T id;
+            //int hours;
+            //hours = 10; //Assignment operator: id = E
+            
+            //Identifier Rules
+            // 1. Unique within scope
+            // 2. Must start with underscore or letter
+            // 3.Consist of letter, digits and underscores
+            // e.g
+            // Valid? firstName, first_Name, first_name
+            // pay1Rate
+            // 1chance
 
-            // Another block
-            // 10 + 45;
-            // NO!!!! { Console.Writeline(); }
+            // Always camel case local variables, noun
+            //Preferred: T id = E;
+            int hours = 10;
+
+            //int pay = 0;
+            //pay = hours * 9;
+            int totalPay = hours * 9;
+
+            // Function overloading - multiple functions with the same name but different parameters
+            // atof, atoi
+            Console.WriteLine(totalPay);
         }
     }
 }
