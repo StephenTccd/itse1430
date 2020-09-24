c8ccb8bfd2a79c541dab3b0a40b8e28fa49229f6
Stephen Nuguid <stephen.nuguid@my.tccd.edu>
2020-09-23 21:27:33 -0500

    Movie Form

---
+++ classwork/MovieLibrary/MovieLibrary.WinformsHost/MovieForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MovieLibrary.WinformsHost
+{
+    public partial class MovieForm : Form
+    {
+        public MovieForm ()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click ( object sender, EventArgs e )
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged ( object sender, EventArgs e )
+        {
+
+        }
+
+        private void label4_Click ( object sender, EventArgs e )
+        {
+
+        }
+
+        private void button2_Click ( object sender, EventArgs e )
+        {
+
+        }
+    }
+}
